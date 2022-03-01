2a18d4a0a887b4f307385638df4305d32369c1db
egbendito <e.gbendito@gmail.com>
2022-03-01 16:33:15 +0000

    update spatial

--- Data_sp.py
+++ Data_sp.py
@@ -40,7 +40,7 @@ years = []
 for l in open(str(i) + str(files[0])):
   y = l.split('   ')[0].replace('\n', '')
   years.append(y)
-  
+
 # Determine the bounding-box of the AOI:
 src = gdal.Open('data/chirps.nc')
 ulx, xres, xskew, uly, yskew, yres  = src.GetGeoTransform()
@@ -51,54 +51,56 @@ drv = gdal.GetDriverByName("GTiff")
 onset = dict.fromkeys(years)
 for k in onset.keys():
   onset[k] = np.zeros(shape=(len(ylen),len(xlen)))
-  drv.Create(str(o) + "onset_" + str(k) + ".tif", len(ylen), len(xlen), 1, gdal.GDT_Float32).SetGeoTransform([ulx, xres, xskew, uly, yskew, -1*yres]) # Need to invert the vertical resolution since we are starting from the top-left corner, and going downwards
+  drv.Create(str(o) + "onset_" + str(k) + ".tif", len(xlen), len(ylen), 1, gdal.GDT_Float32).SetGeoTransform([ulx, xres, 0, uly, 0, yres]) # Need to invert the vertical resolution since we are starting from the top-left corner, and going downwards
 cess = dict.fromkeys(years)
 for k in cess.keys():
   cess[k] = np.zeros(shape=(len(ylen),len(xlen)))
-  drv.Create(str(o) + "cessation_" + str(k) + ".tif", len(ylen), len(xlen), 1, gdal.GDT_Float32).SetGeoTransform([ulx, xres, xskew, uly, yskew, -1*yres]) # Need to invert the vertical resolution since we are starting from the top-left corner, and going downwards
+  drv.Create(str(o) + "cessation_" + str(k) + ".tif", len(xlen), len(ylen), 1, gdal.GDT_Float32).SetGeoTransform([ulx, xres, 0, uly, 0, yres]) # Need to invert the vertical resolution since we are starting from the top-left corner, and going downwards
 leng = dict.fromkeys(years)
 for k in leng.keys():
   leng[k] = np.zeros(shape=(len(ylen),len(xlen)))
-  drv.Create(str(o) + "length_" + str(k) + ".tif", len(ylen), len(xlen), 1, gdal.GDT_Float32).SetGeoTransform([ulx, xres, xskew, uly, yskew, -1*yres]) # Need to invert the vertical resolution since we are starting from the top-left corner, and going downwards
+  drv.Create(str(o) + "length_" + str(k) + ".tif", len(xlen), len(ylen), 1, gdal.GDT_Float32).SetGeoTransform([ulx, xres, 0, uly, 0, yres]) # Need to invert the vertical resolution since we are starting from the top-left corner, and going downwards
 
-# # Process & populate GeoTIFF
+# Process & populate GeoTIFF
 f = 1
 while f <= len(files):
   for y in range(len(ylen)):
     for x in range(len(xlen)):
-        d = open(str(i) + 'RR_GH_' + str(f) + '_ocl.txt')#.read()
-        for l in d.readlines():
-          year = l.split('   ')[0].replace('\n', '')
-          start = l.split('   ')[1].replace('\n', '')
-          onset[str(year)][x][y] = start
-          tif = gdal.Open(str(o) + "onset_" + str(year) + ".tif", gdal.GA_Update)
-          tif.GetRasterBand(1).WriteArray(onset[str(year)])
-          tif.FlushCache()
-        f = f + 1
+      d = open(str(i) + 'RR_' + str(f) + '_ocl.txt')#.read()
+      for l in d.readlines():
+        year = l.split('   ')[0].replace('\n', '')
+        start = l.split('   ')[1].replace('\n', '')
+        onset[str(year)][y][x] = start
+        tif = gdal.Open(str(o) + "onset_" + str(year) + ".tif", gdal.GA_Update)
+        tif.GetRasterBand(1).WriteArray(onset[str(year)])
+        tif.FlushCache()
+      f = f + 1
+
 # Process & populate GeoTIFF
 f = 1
 while f <= len(files):
-  for x in range(len(xlen)):
-    for y in range(len(ylen)):
-        d = open(str(i) + 'RR_GH_' + str(f) + '_ocl.txt')#.read()
+  for y in range(len(ylen)):
+    for x in range(len(xlen)):
+        d = open(str(i) + 'RR_' + str(f) + '_ocl.txt')#.read()
         for l in d.readlines():
           year = l.split('   ')[0].replace('\n', '')
           end = l.split('   ')[2].replace('\n', '')
-          cess[str(year)][x][y] = end
+          cess[str(year)][y][x] = end
           tif = gdal.Open(str(o) + "cessation_" + str(year) + ".tif", gdal.GA_Update)
           tif.GetRasterBand(1).WriteArray(cess[str(year)])
           tif.FlushCache()
         f = f + 1
+
 # Process & populate GeoTIFF
 f = 1
 while f <= len(files):
-  for x in range(len(xlen)):
-    for y in range(len(ylen)):
-        d = open(str(i) + 'RR_GH_' + str(f) + '_ocl.txt')#.read()
+  for y in range(len(ylen)):
+    for x in range(len(xlen)):
+        d = open(str(i) + 'RR_' + str(f) + '_ocl.txt')#.read()
         for l in d.readlines():
           year = l.split('   ')[0].replace('\n', '')
           le = l.split('   ')[2].replace('\n', '')
-          leng[str(year)][x][y] = le
+          leng[str(year)][y][x] = le
           tif = gdal.Open(str(o) + "length_" + str(year) + ".tif", gdal.GA_Update)
           tif.GetRasterBand(1).WriteArray(leng[str(year)])
           tif.FlushCache()
